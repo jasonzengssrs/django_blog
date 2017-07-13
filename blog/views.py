63381b47f5496c8addf919d15ebf3b2606c7ab59
jasonzengssrs <jasonzengssrs@gmial.com>
2017-07-13 16:08:32 +0800

    hello world index

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(requset):
+	return render(requset, 'blog/index.html', context={
+                      'title': 'my blog index', 
+                      'welcome': 'hello world'
+                  })
