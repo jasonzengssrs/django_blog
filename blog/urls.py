52e90b3e01f2c60fac9f11334073947287bfeac8
jasonzengssrs <jasonzengssrs@gmial.com>
2017-07-14 17:50:12 +0800

    markdown and code highlight

--- blog/urls.py
+++ blog/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+
 from . import views
 
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail')
 ]
